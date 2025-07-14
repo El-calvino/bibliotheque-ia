d5d919bfd988702af8da0068649f02ab73e64c13
El-Calvino <kiegaingkouamcalvin@gmail.com>
2025-07-14 14:23:59 -0400

    creation dossier models qui contient les classes livre et humain qui sont appeles dans employes avec des from

---
+++ models/humain.py
@@ -0,0 +1,8 @@
+class Humain:
+    def __init__(self, nom, prenom, adresse, phone, age, statut):       
+        self.nom = nom
+        self.prenom = prenom
+        self.adresse = adresse
+        self.phone = phone
+        self.age = age
+        self.statut = statut
